1d8f9c75b62607e2ba2bf4da04fce40a2d3013bf
Henri Kerola <henri.kerola@kbrax.com>
2023-06-04 11:55:43 -0700

    Q3 changes to train.py

--- homework-2/train.py
+++ homework-2/train.py
@@ -4,6 +4,7 @@ import click
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import mlflow
 
 
 def load_pickle(filename: str):
@@ -19,14 +20,17 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
+    mlflow.autolog()
+
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
